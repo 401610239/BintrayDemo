be1c803ff8a4bdbd5488b864d28ce2d11f6daa0e
401610239 <401610239@qq.com>
2016-08-08 03:44:20 -0700

    20160808

---
+++ bintray/src/main/java/com/gome/bintray/BinTrayTest.java
@@ -0,0 +1,13 @@
+package com.gome.bintray;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by yaodawei on 2016/8/8.
+ */
+public class BinTrayTest {
+    private  void bintraytest(Context context){
+        Toast.makeText(context,"bintraytest",Toast.LENGTH_LONG).show();
+    }
+}
