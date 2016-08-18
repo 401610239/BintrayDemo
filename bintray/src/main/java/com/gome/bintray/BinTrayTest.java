7d4665fa5a6af427799d72338bc2e08d054685c6
401610239 <401610239@qq.com>
2016-08-18 02:25:04 -0700

    上传Bintray

--- bintray/src/main/java/com/gome/bintray/BinTrayTest.java
+++ bintray/src/main/java/com/gome/bintray/BinTrayTest.java
@@ -7,7 +7,7 @@ import android.widget.Toast;
  * Created by yaodawei on 2016/8/8.
  */
 public class BinTrayTest {
-    private  void bintraytest(Context context){
+    public  static void bintraytest(Context context){
         Toast.makeText(context,"bintraytest",Toast.LENGTH_LONG).show();
     }
 }
